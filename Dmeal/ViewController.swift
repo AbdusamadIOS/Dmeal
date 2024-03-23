2bea4b6c2c4f8fc35144584deea60b8469d2da9f
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-03-23 19:45:53 +0500

    Initial Commit

---
+++ Dmeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dmeal
+//
+//  Created by Abdusamad Mamasoliyev on 23/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
